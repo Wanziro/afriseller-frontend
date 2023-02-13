c91391a459bdf8365d7760e366267437a3573850
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-02-13 17:11:41 +0200

    attempt quiz alert

--- src/views/company/right-side/active-quizes/index.js
+++ src/views/company/right-side/active-quizes/index.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Confirmation from "src/components/confirmation";
 import RowsPlaceHolder from "src/components/placeholders/rows";
 import { BACKEND_URL } from "src/constants/app";
 import { errorHandler } from "src/helpers";
 
 function ActiveQuizes({ company }) {
+  const [showAlert, setShowAlert] = useState(false);
   const [quizes, setQuizes] = useState([]);
+  const [selectedQuiz, setSelectedQuiz] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const fetchQuizes = () => {
     if (company?.companyId) {
@@ -29,6 +32,9 @@ function ActiveQuizes({ company }) {
   useEffect(() => {
     fetchQuizes();
   }, [company]);
+
+  const takeQuiz = () => {};
+
   return (
     <div className="companyContentsCard">
       <h3>Active Quizes</h3>
@@ -46,14 +52,39 @@ function ActiveQuizes({ company }) {
                 <b>{item.qTitle}</b>
               </p>
               <small>
-                {item.qFromDate} {item.qFromTime}
+                From: {item.qFromDate} {item.qFromTime}
               </small>
+              <br />
+              <small>
+                Deadline: {item.qToDate} {item.qToTime}
+              </small>
+              <br />
+              <div className="text-end mb-2">
+                <span
+                  style={{
+                    cursor: "pointer",
+                    color: "blue",
+                    textDecoration: "underline",
+                  }}
+                  onClick={() => {
+                    setSelectedQuiz(item);
+                    setShowAlert(true);
+                  }}
+                >
+                  Take quiz
+                </span>
+              </div>
             </div>
           ))}
         </>
       ) : (
         <small>No quiz found</small>
       )}
+      <Confirmation
+        title="Do you want to take this quiz? Click on confirm to take the quiz immediately."
+        setShowAlert={setShowAlert}
+        showAlert={showAlert}
+      />
     </div>
   );
 }
